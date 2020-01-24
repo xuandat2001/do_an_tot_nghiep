53ece2aa8c6ec7402ac016f7e60c38f4761bdec0
xuandat2001 <59912738+xuandat2001@users.noreply.github.com>
2020-01-24 11:06:24 +0700

    add trang_chu

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,69 @@
+<footer>
+            <div class="container">
+                <div class="row ">
+                    <div class="col-xl-3 col-md-6 col-12">
+                        <h3>About Food Coner</h3>
+                        <p class="text-footer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque
+                            elit sit amet lorem fermentum.
+                            <span>Nullam turpis ipsum, dapibus eu congue sit amet.</span>
+                        </p>
+                        <h5>Get In Touch</h5>
+                        <div class="menu-footer">
+                            <ul class="nav nav-menu">
+                                <li class="nav-item item"><span class="fab fa-facebook-f link1"></span></li>
+                                <li class="nav-item item"><span class="fab fa-twitter link2"></span></li>
+                                <li class="nav-item item"><span class="fa fa-rss link3"></span></li>
+                                <li class="nav-item item"><span class="fab fa-vk link4"></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6 col-12">
+                        <h3>Twiter Posts</h3>
+                        <ul>
+                            <li class="text">
+                                Ut aut reiciendis voluptatibus maiores
+                                <a href="#">http://lkj.ewr.com</a>
+                                alias, ut aut reiciendis.
+
+                                <span>
+                                    <i class="fab fa-twitter"></i>
+                                    02 days ago
+                                </span>
+                            </li>
+                            <li class="text">
+                                Itaque earum rerum hic tenetur a sapiente delectus
+                                <a href="#">http://uiubajaj.com</a>
+                                ut aut
+                                voluptatibus.
+                                <span>
+                                    <i class="fab fa-twitter"></i>
+                                    03 days ago
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 col-12">
+                        <h3>Popular Items</h3>
+                        <img src="giao_dien/img/saba-377x251.png" style="width:255px" alt=" cá basa" class="img-footer">
+                    </div>
+                    <div class="col-xl-3 col-sm-6 col-12">
+                        <h3>Newsletter</h3>
+                        <p>Sed pellentesque elit sit amet lorem fermentum, vitae maximus tortor accumsan.</p>
+                        <form action="" method="">
+                            <input type="text" name="email" placeholder="EMAIL" class="form-control input">
+                            <button type="submit" class="btn btn-footer">Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="messege dropdown dropleft float-right">
+                    <a href="#" class="btn dropdown-toggle" data-toggle="dropdown"><img src="giao_dien/img/cach-tat-tu-dong-luu-anh-tren-messenger-100.jpg" alt="messege" class="rounded-circle"></a>
+                    <div class="dropdown-menu">
+                         <a class="dropdown-item" href="#">Liên hệ TAO LÀ ĐẠT</a>
+
+                     </div>
+                </div>
+            </div>
+        </footer>
+        <div class="footer">
+            <p><a href="#">@KOIBENTO</a></p>
+        </div>
